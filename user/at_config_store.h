aa05c17706237412473ef69ed38b1047032d7b3f
atdeveloper <xujingjie@espressif.com>
2014-12-04 10:51:25 +0800

    add user store

--- user/at_config_store.h
+++ user/at_config_store.h
@@ -1,53 +1,68 @@
 #ifndef __AT_CONFIG_STORE_H
 #define __AT_CONFIG_STORE_H
 
-#include <stdint.h>
+//#include <stdint.h>
+#include "c_types.h"
 
-typedef struct {
+typedef struct 
+{
 	int32_t baud_rate;
 	uint8_t byte_size;
 	uint8_t stop_bits;
 	uint8_t parity;
 	uint8_t flow_control;
-}atSerial_t;
+} atSerial_t;
 
-typedef struct {
-	uint8_t dhcp;
+typedef struct 
+{
 	uint8_t ap_mac[6];
-	uint8_t sta_mac[6];
+	uint8_t ap_dhcp;
 	uint8_t ap_ip[4];
+	uint8_t sta_mac[6];
+	uint8_t sta_dhcp;
 	uint8_t sta_ip[4];
+	uint8_t reserve[2];
 // ssid;
 // passwold;
-}atWifi_t;
+} atWifi_t;
 
-typedef struct {
+typedef struct
+{
 	uint8_t link_type;
-	union {
+	uint8_t remote_type;
+	union
+	{
 		char remote_domain[64];
 		uint8_t remote_ip[4];
 	} remote_target;
 	int32_t remote_port;
 	int32_t local_port;
-}atClient_t;
+	uint8_t reserve[2];
+} atClient_t;
 
-typedef struct {
+typedef struct 
+{
 	int32_t local_port;
-}atServer_t;
+	uint8_t reserve[4];
+} atServer_t;
 
-typedef struct {
-	uint8_t auto_wifi;
+typedef struct 
+{
 	uint8_t auto_trans;
-	union {
+	uint8_t role_type;
+	union
+	{
 		atClient_t client;
 		atServer_t server;
 	} link_role;
-}atStart_t;
+	uint8_t reserve[2];
+} atStart_t;
 
-typedef struct {
+typedef struct 
+{
 	atSerial_t serial;
 	atWifi_t wifi_set;
 	atStart_t start;
-}atConfig_t;
+} atConfig_t;
 
 #endif //__AT_CONFIG_STORE_H
