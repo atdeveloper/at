814847477314a4cd4e70925da73f4ad1814f387f
atdeveloper <xujingjie@espressif.com>
2014-12-08 09:39:15 +0800

    add head

--- user/esp_common.c
+++ user/esp_common.c
@@ -1,3 +1,20 @@
+/*
+ * File	: esp_common.c
+ * This file is part of Espressif's AT+ command set program.
+ * Copyright (C) 2013 - 2016, Espressif Systems
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of version 3 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include "c_types.h"
 #include "spi_flash.h"
 #include "esp_common.h"
